e55be3374364bcf2337f734589b740aa06a28935
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-05-16 12:18:16 -0500

    improve search screen

--- pages/patient/finishProfile/+Page.tsx
+++ pages/patient/finishProfile/+Page.tsx
@@ -4,7 +4,7 @@ import ProfileForm from "@/pages/patient/finishProfile/ProfileForm";
 
 function Page() {
   return (
-    <Container fluid mt={"2rem"} pb={"5rem"}>
+    <Container fluid mt={"1.5rem"} pb={"5rem"}>
       <Center>
         <ProfileForm/>
       </Center>
